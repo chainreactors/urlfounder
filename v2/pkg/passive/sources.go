d981461abdbe9320f9cb3ec013788016b463b7e6
M09Ic <m09ician@gmail.com>
2023-04-04 18:52:19 +0800

    init

---
+++ v2/pkg/passive/sources.go
@@ -0,0 +1,83 @@
+package passive
+
+import (
+	"fmt"
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/webarchive"
+	"strings"
+
+	"golang.org/x/exp/maps"
+
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping"
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/zoomeye"
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping/sources/zoomeyeapi"
+)
+
+var AllSources = [...]subscraping.Source{
+	&zoomeye.Source{},
+	&zoomeyeapi.Source{},
+	&webarchive.Source{},
+	// &threatminer.Source{}, // failing  api
+	// &reconcloud.Source{}, // failing due to cloudflare bot protection
+}
+
+var NameSourceMap = make(map[string]subscraping.Source, len(AllSources))
+
+func init() {
+	for _, currentSource := range AllSources {
+		NameSourceMap[strings.ToLower(currentSource.Name())] = currentSource
+	}
+}
+
+// Agent is a struct for running passive subdomain enumeration
+// against a given host. It wraps subscraping package and provides
+// a layer to build upon.
+type Agent struct {
+	sources []subscraping.Source
+}
+
+// New creates a new agent for passive subdomain discovery
+func New(sourceNames, excludedSourceNames []string, useAllSources, useSourcesSupportingRecurse bool) *Agent {
+	sources := make(map[string]subscraping.Source, len(AllSources))
+
+	if useAllSources {
+		maps.Copy(sources, NameSourceMap)
+	} else {
+		if len(sourceNames) > 0 {
+			for _, source := range sourceNames {
+				if NameSourceMap[source] == nil {
+					gologger.Warning().Msgf("There is no source with the name: %s", source)
+				} else {
+					sources[source] = NameSourceMap[source]
+				}
+			}
+		} else {
+			for _, currentSource := range AllSources {
+				if currentSource.IsDefault() {
+					sources[currentSource.Name()] = currentSource
+				}
+			}
+		}
+	}
+
+	if len(excludedSourceNames) > 0 {
+		for _, sourceName := range excludedSourceNames {
+			delete(sources, sourceName)
+		}
+	}
+
+	if useSourcesSupportingRecurse {
+		for sourceName, source := range sources {
+			if !source.HasRecursiveSupport() {
+				delete(sources, sourceName)
+			}
+		}
+	}
+
+	gologger.Debug().Msgf(fmt.Sprintf("Selected source(s) for this search: %s", strings.Join(maps.Keys(sources), ", ")))
+
+	// Create the agent, insert the sources and remove the excluded sources
+	agent := &Agent{sources: maps.Values(sources)}
+
+	return agent
+}
---
+++ v2/pkg/subscraping/sources/webarchive/webarchive.go
@@ -0,0 +1,102 @@
+package webarchive
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/projectdiscovery/subfinder/v2/pkg/subscraping"
+	"net/http"
+	"time"
+)
+
+// search results
+type webarchiveResults struct {
+	Matches []string
+}
+
+// Source is the passive scraping agent
+type Source struct {
+	timeTaken time.Duration
+	errors    int
+	results   int
+	skipped   bool
+}
+
+// Run function returns all subdomains found with the service
+func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Session) <-chan subscraping.Result {
+	results := make(chan subscraping.Result)
+	s.errors = 0
+	s.results = 0
+
+	go func() {
+		defer func(startTime time.Time) {
+			s.timeTaken = time.Since(startTime)
+			close(results)
+		}(time.Now())
+
+		headers := map[string]string{
+			"Accept":       "application/json",
+			"Content-Type": "application/json",
+		}
+		api := fmt.Sprintf("https://web.archive.org/web/timemap/json?url=%s&matchType=prefix&collapse=urlkey&output=json&fl=original%%2Cmimetype%%2Ctimestamp%%2Cendtimestamp%%2Cgroupcount%%2Cuniqcount%%2Cstatuscode&limit=1000", domain)
+
+		resp, err := session.Get(ctx, api, "", headers)
+		isForbidden := resp != nil && resp.StatusCode == http.StatusForbidden
+		if err != nil {
+			if !isForbidden {
+				results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+				s.errors++
+				session.DiscardHTTPResponse(resp)
+			}
+			return
+		}
+
+		var res [][]string
+		err = json.NewDecoder(resp.Body).Decode(&res)
+		if err != nil {
+			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Error, Error: err}
+			s.errors++
+			resp.Body.Close()
+			return
+		}
+		resp.Body.Close()
+
+		for _, r := range res[1:] {
+			results <- subscraping.Result{Source: s.Name(), Type: subscraping.Subdomain, Value: r[0]}
+			s.results++
+		}
+
+	}()
+
+	return results
+}
+
+// Name returns the name of the source
+func (s *Source) Name() string {
+	return "webarchive"
+}
+
+func (s *Source) IsDefault() bool {
+	return false
+}
+
+func (s *Source) HasRecursiveSupport() bool {
+	return false
+}
+
+func (s *Source) AddApiKeys(keys []string) {
+
+}
+
+func (s *Source) NeedsKey() bool {
+	return true
+}
+
+func (s *Source) Statistics() subscraping.Statistics {
+	return subscraping.Statistics{
+		Errors:    s.errors,
+		Results:   s.results,
+		TimeTaken: s.timeTaken,
+		Skipped:   s.skipped,
+	}
+}
